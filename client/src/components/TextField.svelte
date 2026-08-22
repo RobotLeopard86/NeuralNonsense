c7270f81d9c46bc828bdf7433481e31828fd6fc0
RobotLeopard86 <owensiebers@icloud.com>
2026-08-22 10:36:06 -0700

    Finish home screen view

---
+++ client/src/components/TextField.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	interface Props {
+		value: string;
+		placeholder: string;
+		class?: string;
+		maxLength?: number;
+	}
+
+	let { value = $bindable(), ...props }: Props = $props();
+</script>
+
+<input type="text" bind:value maxlength={props.maxLength || 1000} placeholder={props.placeholder} class={'focus:outline-none px-8 py-4 rounded-2xl text-lg bg-white/5 border-2 border-white/10 text-white placeholder:text-white/30 ' + (props.class || '')} />
--- client/src/routes/Home.svelte
+++ client/src/routes/Home.svelte
@@ -1,30 +1,60 @@
 <script lang="ts">
 	import { fly } from 'svelte/transition';
-	import { ArrowLeftOutline } from 'flowbite-svelte-icons';
+	import { ArrowLeftOutline, ArrowRightOutline } from 'flowbite-svelte-icons';
 
 	import Logo from '../components/Logo.svelte';
 	import NeonButton from '../components/NeonButton.svelte';
+	import TextField from '../components/TextField.svelte';
 
-	type Phase = 'Initial' | 'Code' | 'Name';
+	type Phase = 'Initial' | 'Join' | 'Create';
 	let phase: Phase = $state('Initial');
+
+	let playerName = $state('');
+	let roomCode = $state('');
+	$effect(() => {
+		roomCode = roomCode.toUpperCase();
+	});
+
+	let canJoin = $derived(playerName && roomCode && roomCode.length === 4);
 </script>
 
 <Logo />
 <p class="text-center text-white/50 text-sm mb-8">Can you out-funny an AI?</p>
 
-<main class="mb-auto grid">
+<main class="mb-auto w-full flex flex-col items-center px-4">
 	{#if phase == 'Initial'}
-		<div in:fly={{ duration: 500, x: 1000 }} out:fly={{ duration: 500, x: -1000 }} class="[grid-area:1/1]">
-			<NeonButton variant="Cyan" onclick={() => (phase = 'Code')} class="w-full m-4">Join an Existing Game</NeonButton>
-			<NeonButton variant="Red" onclick={() => (phase = 'Name')} class="w-full m-4">Create a New Game</NeonButton>
+		<NeonButton variant="Cyan" onclick={() => (phase = 'Join')} class="w-full max-w-md my-4">Join an Existing Game</NeonButton>
+		<NeonButton variant="Red" onclick={() => (phase = 'Create')} class="w-full max-w-md my-4">Create a New Game</NeonButton>
+	{:else if phase == 'Join'}
+		<div class="flex flex-col max-w-md w-full">
+			<button class="text-left text-white/50 text-sm mb-8 flex flex-row hover:text-white/75 transition-colors" onclick={() => (phase = 'Initial')}><ArrowLeftOutline size="md" /> Back</button>
+			<p class="text-left text-white/50 text-md mb-2">YOUR NAME</p>
+			<TextField placeholder="What will you call yourself?" bind:value={playerName} class="w-full max-w-md" maxLength={20} />
+			<br />
+			<p class="text-left text-white/50 text-md mb-2">ROOM CODE</p>
+			<input type="text" maxlength="4" placeholder="CODE" bind:value={roomCode} class="w-full max-w-md focus:outline-none px-8 py-4 rounded-2xl text-lg bg-white/5 border-2 border-white/10 text-white placeholder:text-white/30 font-bold text-center tracking-[1em] placeholder:tracking-normal placeholder:font-normal" />
+			<br />
+			<NeonButton
+				variant="Cyan"
+				onclick={() => {
+					alert(`You joined game ${roomCode} as ${playerName}!`);
+				}}
+				class="w-full max-w-md my-4"
+				disabled={!canJoin}>Join Game <ArrowRightOutline size="xl" /></NeonButton>
 		</div>
-	{:else if phase == 'Code'}
-		<div in:fly={{ duration: 500, x: 1000 }} out:fly={{ duration: 500, x: -1000 }} class="[grid-area:1/1]">
-			<NeonButton variant="Translucent" onclick={() => (phase = 'Initial')} class="w-full m-4"><ArrowLeftOutline size="xl" /> Back</NeonButton>
-		</div>
-	{:else if phase == 'Name'}
-		<div in:fly={{ duration: 500, x: 1000 }} out:fly={{ duration: 500, x: -1000 }} class="[grid-area:1/1]">
-			<NeonButton variant="Translucent" onclick={() => (phase = 'Initial')} class="w-full m-4"><ArrowLeftOutline size="xl" /> Back</NeonButton>
+	{:else if phase == 'Create'}
+		<div class="flex flex-col max-w-md w-full">
+			<button class="text-left text-white/50 text-sm mb-8 flex flex-row hover:text-white/75 transition-colors" onclick={() => (phase = 'Initial')}><ArrowLeftOutline size="md" /> Back</button>
+			<p class="text-left text-white/50 text-md mb-2">YOUR NAME</p>
+			<TextField placeholder="What will you call yourself?" bind:value={playerName} class="w-full max-w-md" maxLength={20} />
+			<br />
+			<NeonButton
+				variant="Red"
+				onclick={() => {
+					alert(`You created a new game as ${playerName}!`);
+				}}
+				class="w-full max-w-md my-4"
+				disabled={!playerName}>Create <ArrowRightOutline size="xl" /></NeonButton>
 		</div>
 	{/if}
 </main>
